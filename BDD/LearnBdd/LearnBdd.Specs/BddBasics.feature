86dddb15724de0e2e4c2ad00ee3e0d13f965ab8c
zzt108 <zzt@outlook.hu>
2015-03-04 12:53:13 +0100

    Created BDD using MSTest

---
+++ BDD/LearnBdd/LearnBdd.Specs/BddBasics.feature
@@ -0,0 +1,13 @@
+﻿Feature: BddBasics
+	In order to find out how Bdd works
+	As a QA Engineer
+	I want to see BDD basic working
+
+@mytag
+Scenario: The answer with the highest vote gets to the top
+	Given there is a question "What`s your favorite colour?" with the answers
+		| Answer         | Vote |
+		| Red            | 1    |
+		| Cucumber green | 1    |
+	When you upvote answer "Cucumber green"
+	Then the answer "Cucumber green" should be on top
---
+++ BDD/LearnBdd/LearnBdd.Specs/BddBasicsSteps.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace LearnBdd.Specs
+{
+  [Binding]
+  public class BddBasicsSteps
+  {
+    [Given(@"there is a question ""(.*)"" with the answers")]
+    public void GivenThereIsAQuestionWithTheAnswers(string p0, Table table)
+    {
+      Console.WriteLine("Question {0}", p0);
+      foreach (var row in table.Rows)
+      {
+        Console.WriteLine("Row {0}", row.ToString());
+      }
+      //ScenarioContext.Current.Pending();
+    }
+
+    [When(@"you upvote answer ""(.*)""")]
+    public void WhenYouUpvoteAnswer(string p0)
+    {
+      ScenarioContext.Current.Pending();
+    }
+
+    [Then(@"the answer ""(.*)"" should be on top")]
+    public void ThenTheAnswerShouldBeOnTop(string p0)
+    {
+      ScenarioContext.Current.Pending();
+    }
+  }
+}
