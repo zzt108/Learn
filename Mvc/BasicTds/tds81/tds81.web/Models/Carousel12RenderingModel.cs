0b1e6239629866227fe3e95021aaf365d62d628b
zzt108 <zzt@outlook.hu>
2015-05-24 17:17:13 +0200

    Using rendering model class

---
+++ Mvc/BasicTds/tds81/tds81.web/Models/Carousel12RenderingModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Presentation;
+
+namespace tds81.web.Models
+{
+    public class Carousel12RenderingModel : Sitecore.Mvc.Presentation.RenderingModel
+    {
+      public override void Initialize(Rendering rendering)
+      {
+        base.Initialize(rendering);
+        CarouselSlides =
+          Item.Children.ToList(); //.Select(id => Item.Database.GetItem(id)).ToList();
+      }
+
+      public IList<Item> CarouselSlides { get; private set; }
+    }
+}
